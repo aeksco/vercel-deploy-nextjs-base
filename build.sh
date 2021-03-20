bb8fed42833208a78dac8380e242059ade626eb9
Alexander Schwartzberg <aeksco@gmail.com>
2021-03-20 19:54:32 -0400

    Simplified build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/aeksco/vercel-deploy-nextjs-plugin.git
-yarn --cwd ~/test_project/ install
-yarn --cwd ~/test_project/ build
+# yarn --cwd ~/test_project/ install
+# yarn --cwd ~/test_project/ build
 yarn build
