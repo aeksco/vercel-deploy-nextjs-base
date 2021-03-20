48db0ede277a912daae5b3811715ada3588a8103
Alexander Schwartzberg <aeksco@gmail.com>
2021-03-20 19:51:29 -0400

    Added build.sh + plugin import

---
+++ build.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/aeksco/vercel-deploy-nextjs-plugin.git
+yarn --cwd ~/test_project/ install
+yarn --cwd ~/test_project/ build
+yarn build
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import Head from "next/head";
 import { Navbar } from "../src/components/Navbar";
 import { AppProps } from "next/app";
 
+// @ts-ignore
+import * as plugin from "../vercel-deploy-nextjs-plugin/dist/plugin.json";
+
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <>
@@ -11,6 +14,7 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
             </Head>
             <Navbar />
             <Component {...pageProps} />
+            <pre>{JSON.stringify({ plugin }, null, 4)}</pre>
         </>
     );
 }
