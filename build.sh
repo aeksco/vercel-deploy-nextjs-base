d0b8b518d47ae82ce803caf04e854172b7a5c342
Alexander Schwartzberg <aeksco@gmail.com>
2021-03-20 20:22:29 -0400

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-git clone https://github.com/aeksco/vercel-deploy-nextjs-plugin.git ./plugin
+git clone https://github.com/aeksco/vercel-deploy-nextjs-plugin.git ./plugin-repo
+cp -rT ./plugin-repo ./plugin
 ls -la ./plugin
 yarn --cwd ./vercel-deploy-nextjs-plugin install
 yarn --cwd ./vercel-deploy-nextjs-plugin build
